e228d2ff8b219c05d13ee50baa43c3f22cf15da1
nicolasbolt <nicolas.bolt@gmail.com>
2020-05-10 17:59:18 -0400

    Added UpdateView & DeleteView.  Closes #11

--- yt/videos/urls.py
+++ yt/videos/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.CreateVideo.as_view(), name='video-create'),
-    path('<int:pk>/', views.DetailVideo.as_view(), name='video-detail')
+    path('<int:pk>/', views.DetailVideo.as_view(), name='video-detail'),
+    path('<int:pk>/edit', views.UpdateVideo.as_view(), name='video-update'),
+    path('<int:pk>/delete', views.DeleteVideo.as_view(), name="video-delete"),
 ]
--- yt/videos/views.py
+++ yt/videos/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Video
+
+def index(request):
+    return render(request, 'videos/index.html')
 
 class CreateVideo(CreateView):
     model = Video
@@ -15,4 +18,20 @@ class CreateVideo(CreateView):
 class DetailVideo(DetailView):
     model = Video
     template_name = 'videos/detail_video.html'
+
+class UpdateVideo(UpdateView):
+    model = Video
+    fields = ['title', 'description']
+    template_name = "videos/create_video.html"
+
+    def get_success_url(self):
+        return reverse('video-detail', kwargs={'pk': self.object.pk})
+
+class DeleteVideo(DeleteView):
+    model = Video
+    template_name = 'videos/delete_video.html'
+
+    def get_success_url(self):
+        return reverse('index')
+
 
