a9f85cac708020d0ce39dd749cc4e6df39cf91b4
nicolasbolt <nicolas.bolt@gmail.com>
2020-05-16 14:50:20 -0400

    Added user test mixin, login required.  Closes #5

--- yt/videos/models.py
+++ yt/videos/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.TextField()
     videoFile = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['mp4'])])
+    date_posted = models.DateTimeField(default=timezone.now)
