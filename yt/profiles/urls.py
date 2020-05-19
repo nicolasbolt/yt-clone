136c9450c3031a36a41b7ce7c166a393d11e113f
nicolasbolt <nicolas.bolt@gmail.com>
2020-05-19 17:18:26 -0400

    Profile is now a DetailView and url is based on pk.  Closes #29

--- yt/profiles/urls.py
+++ yt/profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.ProfileView.as_view(), name='profile'),
+    path('user/<int:pk>', views.ProfileView.as_view(), name='profile'),
 ]
--- yt/profiles/views.py
+++ yt/profiles/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views import View
+# from django.views import View
+from django.views.generic.detail import DetailView
+from .models import Profile
 
-class ProfileView(View):
+# class ProfileView(View):
 
-    def get(self, request, *args, **kwargs):
-        return render(request, 'profiles/profile.html')
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'profiles/profile.html')
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = 'profiles/profile.html'
+
